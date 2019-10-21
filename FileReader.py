a12effa34b269d7633a290322c9c23ef13dd27ff
colinsandberg <cmsandbe@uvm.edu>
2019-10-21 15:31:11 -0400

    Added file to read in comments from csv's

--- FileReader.py
+++ FileReader.py
@@ -7,6 +7,9 @@ import random
 notbanned= pd.read_csv("notbanned.csv", delimiter=',')
 
 pd.set_option('display.max_columns', 5)  # Set to actually print out the full columns, change if needed
-print(notbanned.head(n=1))
-for line in notbanned:
-    print(line)
+print(notbanned.head(n=10))
+
+banned = pd.read_csv("banned.csv", delimiter=',')
+
+pd.set_option('display.max_columns', 5)  # Set to actually print out the full columns, change if needed
+print(banned.head(n=10))
