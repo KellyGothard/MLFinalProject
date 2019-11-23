7756613fbb733e487edbbf05848120a67976ba9b
David Matthews <David.Matthews.1@uvm.edu>
2019-11-23 11:25:25 -0500

    updates to createcsv_script

--- CreateCSV.py
+++ CreateCSV.py
@@ -10,33 +10,26 @@ import ujson
 import time
 from string import punctuation
 import sys
-
+from nltk.tokenize import RegexpTokenizer
 import pprint
 
 
 # In[2]:
 
+in_name = sys.argv[1]
+directories = in_name.split("/")
+dir = '/'.join(directories[:-1])
+fname = directories[-1]
 
-REDDIT_FILE = "%s.bz2"%sys.argv[1]
-CSV_FILE = "%s.csv" % sys.argv[1]
+REDDIT_FILE = "%s.bz2" % in_name
+CSV_FILE = "CSV/%s.csv" % fname
+TXT_banned = "CSV/%s_banned.txt" % fname
+TXT_not_banned = "CSV/%s_notbanned.txt" %fname
 
 NUM_POSTS_TO_READ = -1
 num_read = 0
 
-
-def clean_text(text):
-    exclude = set(punctuation) # Keep a set of "bad" characters.
-    list_letters_noPunct = [ char for char in text if char not in exclude ]
-    
-    # Now we have a list of LETTERS, *join* them back together to get words:
-    text_noPunct =  "".join(list_letters_noPunct)
-
-    # Split this big string into a list of words:
-    list_words = text_noPunct.strip().split()
-    
-    # Convert to lower-case letters:
-    list_words = [ word.lower() for word in list_words ]
-    return list_words
+tokenizer = RegexpTokenizer(r"\w+")
 
 
 
@@ -48,11 +41,21 @@ with open(BANNED_LOC) as f:
 
 
 f = open(CSV_FILE, 'w', newline='')
+banned_file = open(TXT_banned, "w")
+notbanned_file = open(TXT_not_banned, "w")
+
 writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
-writer.writerow(["id10","datetime", "subreddit", "author", "body", "banned", "suspect"])
+writer.writerow(["id10","datetime", "subreddit", "author", "body", "tokens",  "banned", "suspect"])
 
 def insert(post):
-    writer.writerow([post["id10"], post["created_utc"], post["subreddit"], post["author"], post["body"], post["banned"], post["suspect"]])
+    writer.writerow([post["id10"], post["created_utc"], post["subreddit"], post["author"], post["body"], post["tokens"], post["banned"], post["suspect"]])
+    tokens = post["tokens"].split()
+    if post["banned"]:
+        for token in tokens:
+            banned_file.write(token + "\n")
+    else:
+        for token in tokens:
+            notbanned_file.write(token + "\n")
 
 
 # In[11]:
@@ -70,8 +73,7 @@ for line in f:
     post["banned"] = True if post["subreddit"] in banned_dict else False
     post["suspect"] = True if post["body"] == "[removed]" or post["body"] == "[deleted]" else False
     post["id10"] = int(post["id"], 36)
-#    cleaned_text = clean_text(post["body"])
-#    post["bodySan"] = ' '.join(cleaned_text)
+    post["tokens"] = ' '.join(tokenizer.tokenize(post["body"]))
     insert(post)
     if num_read%int(1e4) == 0:
         print("Read in: %10dK posts in %.2fs (%.2fs)"%(num_read//int(1e3), time.time() - t0, time.time() - t1), flush=True)
